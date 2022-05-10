be85059b4acd67e170cba4ecdfe05cc3eb6acfe7
Marcus <marcus.kaitara.lorenzo@gmail.com>
2022-05-09 21:17:26 -0700

    add styling, alignment

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {postInputToOpenAI} from "./../src/js/apiData.js";
 function dataWrapper() {
   
   //user input storage for POST requests.
+  // let userName = document.getElementById("userName").value;
   let userInput = {
     prompt: "Write a poem about a dog wearing skis", //document.getElementById("textInput").value;
     temperature: 0.5,
@@ -21,29 +22,35 @@ function dataWrapper() {
         const returnedDataText = returnedData.choices[0].text;
         responseArray.push(returnedDataText);
         console.log(responseArray);
-        prependToPage(returnedDataText);
+        prependToPage(returnedDataText, "right");
       });
   };
 
   const submitButton = document.getElementById("submitBtn");
   submitButton.addEventListener("click", (event) =>{
     event.preventDefault();
-    submitEvent(userInput);
+    prependToPage(userInput.prompt, "left");
+    //Add "...AI is typing text?"
+    setTimeout(submitEvent(userInput), 900);
   });
 
 
-  /* UI */
+  /* MAIN UI */
   let postID = 0;
-  const prependToPage = (textToPrepend) => {
+  const prependToPage = (textToPrepend, side) => {
     //General selectors
     const output = document.querySelector(".output");
     const chatBubbleDiv = document.createElement("div");
     let newPostID = postID ++;
     let postIndex = responseArray.length - 1;
+    
+    //Time
+    const timestamp = new Date().toLocaleTimeString();
 
     //create new div for each new post, attach ID to length.
-    chatBubbleDiv.classList.add(`chatBubble`);
+    chatBubbleDiv.classList.add(`chatBubble-${side}`);
     chatBubbleDiv.setAttribute(`id`, `post${newPostID}`);
+    chatBubbleDiv.style.justifyContent = side;
     output.appendChild(chatBubbleDiv);
 
     // for each new post, add a delete button
@@ -62,9 +69,10 @@ function dataWrapper() {
     const p = document.createElement("p");
     const length = document.createElement("p");
     p.textContent = textToPrepend;
-    length.textContent =  `Message ID: ${newPostID}`;
+    length.textContent =  `Message ID: ${newPostID}    Sent at ${timestamp}`;
     chatBubbleDiv.prepend(p);
     p.appendChild(length);
+
   };
 
   // const clearAll = () =>{
