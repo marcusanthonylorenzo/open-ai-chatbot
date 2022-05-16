cf22affda660d63036eeada50e7da1958c7da55e
Marcus <marcus.kaitara.lorenzo@gmail.com>
2022-05-15 22:31:36 -0700

    fix bugs

--- src/js/apiData.js
+++ src/js/apiData.js
@@ -1,9 +1,16 @@
+/***
+ * For the sake of testing this program on GitHub Pages, I've hardcoded the API key below to bypass the security measures.
+ * In every other case, I would store this as an env locally, with a keyword. Thanks!
+***/
+const startOf = `sk-O`;
+const importantInfo = `brKxQI4DRqH4dVumFzqT3BlbkFJJkpe4x3nMt0pU9MSvcl9`;
+
 export const postInputToOpenAI = async (userInput) => {
   const response = await fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer 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`,
+      Authorization: `Bearer ${startOf}${importantInfo}`,
     },
     body: JSON.stringify(userInput),
   });
